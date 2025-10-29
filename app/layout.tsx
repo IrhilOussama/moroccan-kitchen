546c8d9ff4c1d6c8d0a0d926ea7197e46a866fd8
IrhilOussama <oussama.irhil@usmba.ac.ma>
2025-10-30 00:36:25 +0100

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="po68qYj20e7v7O_OwNOPiXqiLtLQHq0DBxUbGlpBScU" />
+      </head>
       <body className={`${inter.className} bg-amber-50`}>
         <LanguageProvider>
           <Navbar />
